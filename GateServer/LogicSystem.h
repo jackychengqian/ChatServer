d65a60d14c304c30c0640c02a0f8157cfaf997c1
qc <2458144519@qq.com>
2024-11-24 15:59:21 +0800

    添加项目文件。

---
+++ GateServer/LogicSystem.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Singleton.h"
+#include "const.h"
+#include <map>
+#include <functional>
+
+class HttpConnection;
+using HttpHandler = std::function<void(std::shared_ptr<HttpConnection>)> ;
+class LogicSystem : public Singleton<LogicSystem>
+{
+	friend class Singleton<LogicSystem>;
+public:	
+	~LogicSystem();
+	bool HandleGet(std::string, std::shared_ptr<HttpConnection>);
+	void RegGet(std::string url, HttpHandler handler);
+private:
+	LogicSystem();
+	std::map<std::string, HttpHandler> _post_handlers;
+	std::map<std::string, HttpHandler> _get_handlers;
+};
+
