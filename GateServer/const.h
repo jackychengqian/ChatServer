967eda1879dd7a698fefe0b4a0c67aa3bf90ebef
qc <2458144519@qq.com>
2024-11-25 22:32:57 +0800

    grpc实现verifyclient

--- GateServer/const.h
+++ GateServer/const.h
@@ -5,17 +5,24 @@
 #include <iostream>
 #include <memory>
 #include <unordered_map>
+#include <json/json.h>
+#include <json/value.h>
+#include <json/reader.h>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+
+
 namespace beast = boost::beast;         // from <boost/beast.hpp>
 namespace http = beast::http;           // from <boost/beast/http.hpp>
 namespace net = boost::asio;            // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
-
-#include <json/json.h>
-#include <json/value.h>
-#include <json/reader.h>
 
 enum ErrorCodes {
     Success = 0,
     Error_Json = 1001,  //JsonΩ‚Œˆ¥ÌŒÛ
     RPCFailed = 1002,  //RPC«Î«Û¥ÌŒÛ
 };
+
+class ConfigMgr;
+extern ConfigMgr gCfgMgr;
