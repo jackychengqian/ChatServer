99e65c9f7d0655f703c9fcae2ed8fc55e924d2ca
qc <2458144519@qq.com>
2024-12-17 19:22:05 +0800

    添加重置密码功能

--- GateServer/MysqlMgr.h
+++ GateServer/MysqlMgr.h
@@ -8,6 +8,11 @@ class MysqlMgr : public Singleton<MysqlMgr>
 public:
     ~MysqlMgr();
     int RegUser(const std::string& name, const std::string& email, const std::string& pwd);
+    bool CheckEmail(const std::string& name, const std::string& email);
+    
+    bool UpdatePwd(const std::string& name, const std::string& newpwd);
+    bool CheckPwd(const std::string& email, const std::string& pwd, UserInfo& userInfo);
+    bool TestProcedure(const std::string& email, int& uid, std::string& name);
 private:
     MysqlMgr();
     MysqlDao  _dao;
